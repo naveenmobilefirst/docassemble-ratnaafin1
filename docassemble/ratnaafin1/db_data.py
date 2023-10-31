7d2d52c73a618c9f719f98580c9dad723b7a2e0a
Naveen Boddeda <148537170+naveenmobilefirst@users.noreply.github.com>
2023-10-31 11:03:12 -0400

    blu updated

--- docassemble/ratnaafin1/db_data.py
+++ docassemble/ratnaafin1/db_data.py
@@ -1,5 +1,6 @@
 from docassemble.base.util import variables_snapshot_connection,get_config, user_info, json
 import psycopg2
+from num2words import num2words
 from cryptography.fernet import Fernet
 from docassemble.base.util import log
 from docassemble.base.util import *
@@ -11,3 +12,11 @@ def branch_addres(branch_place):
   results = [record[0] for record in cur.fetchall()]
   conn.close()
   return results
+
+
+
+def number_to_indian_words(number):
+    return num2words(number, lang='en_IN')
+
+
+
